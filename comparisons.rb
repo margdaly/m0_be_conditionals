22bb2185f42cd5c1219a7212809de088c1ab8516
margdaly <margaret.daly@gmail.com>
2023-01-04 15:47:15 -0500

    Answer prompts in part 3

--- comparisons.rb
+++ comparisons.rb
@@ -106,15 +106,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
+puts loves_to_play && loves_treats
 
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age < 2
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
